3aab8334a594358799640d6110b3fd471b6cdc7a
rahul0x00 <rahul0x00@gmail.com>
2023-08-15 10:00:00 +0530

    Error Handler

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const btn = document.querySelector(".btn");
 const txtarea = document.querySelector(".text--input");
 const output = document.querySelector(".output");
+const errorMsg = document.querySelector(".errorMessage");
 
 document.addEventListener("click", function () {
   const backgroundMusic = document.getElementById("background-music");
@@ -11,20 +12,25 @@ document.addEventListener("click", function () {
 
 const content = function (data) {
   console.log(data);
-  output.innerHTML = data;
+  output.innerText = data;
 };
+function errorHandler(error) {
+  errorMsg.innerText =
+    "We are sorry. You can only use this for 10 times per hour. Try again later.";
+}
 btn.addEventListener("click", () => {
+  errorMsg.innerText = "";
+  output.innerText = "";
   //Getting the message from textarea
   const message = txtarea.value;
-  console.log("button clicked");
-  console.log(message);
+  if (message === "") errorMsg.innerText = "Please enter some text";
   fetch(
     `https://api.funtranslations.com/translate/mandalorian.json?text=${
       encodeURIComponent(message)
     }`,
   ).then((response) => response.json()).then((data) =>
     content(data.contents.translated) // output.innnerHTML`${data}`
-  );
+  ).catch(errorHandler);
 });
 // audioElement.play();
 //
